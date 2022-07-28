2eec33eb5b67c745dcc4f05571c3c771809278f5
camisarp <camilarp.rec@gmail.com>
2022-07-27 22:35:08 -0300

    :recycle: Refactor route

--- src/routes/petShopsAndSpecialtyHousesRoutes.js
+++ src/routes/petShopsAndSpecialtyHousesRoutes.js
@@ -3,9 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/petShopsAndSpecialtyHouses", controller.getAllPetShopsAndSpecialtyHouses);
-router.get("/petShopsAndSpecialtyHouses/address", controller.getByAddress);
 router.get("/petShopsAndSpecialtyHouses/name", controller.getByName);
 router.get("/petShopsAndSpecialtyHouses/classification", controller.getByClassification);
+router.get("/petShopsAndSpecialtyHouses/address", controller.getByAddress);
 router.get("/petShopsAndSpecialtyHouses/:id", controller.getPetShopsAndSpecialtyHousesById);
 
 router.post("/petShopsAndSpecialtyHouses", controller.  createNewPetShopsAndSpecialtyHouses);
